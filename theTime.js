0fa8abca04955301389cfc1f116f2ccadce3366d
Cal Corbin <calcorbin@gmail.com>
2016-02-08 18:08:51 -0600

    The Hex Clock
    This clock displays the current time in hex code which also happens to
    change the background color of the page to the corresponding hex code.
    The clock is in military time.

---
+++ theTime.js
@@ -0,0 +1,19 @@
+var d, h, m, s, color;
+// displays time in military time that also acts as a hexidecimal code
+function displayTime() {
+	d = new Date();
+	h = d.getHours();
+	m = d.getMinutes();
+	s = d.getSeconds();
+	
+	//adds zero to left of numbers
+	if(h <= 9) h = '0'+ h;
+	if(m <= 9) m = '0'+ m;
+	if(s <= 9) s = '0' + s;
+
+	color = "#"+h+m+s;
+	document.body.style.background = color;
+	document.getElementById("hex").innerHTML = color;
+	setTimeout(displayTime, 1000);
+}
+displayTime();
